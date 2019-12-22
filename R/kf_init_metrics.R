70f6f405cea603c3826aa7cf7a7ad1d3ad95ff2f
Nick DiQuattro <ndiquattro@gmail.com>
2019-12-21 19:06:14 -0800

    adds kf_init_metrics

---
+++ R/kf_init_metrics.R
@@ -0,0 +1,13 @@
+#' Initialize Pipeline Metrics Artifacts
+#'
+#' Lay the foundational elements of the JSON expected by Kubeflow to provide metrics from your components.
+#'
+#' @return A list
+#' @export
+#'
+#' @examples
+#' metrics <- kf_init_metrics()
+#' jsonlite::toJSON(metrics, pretty = TRUE)
+kf_init_metrics <- function() {
+  structure(list(metrics = list()), class = "kflow_meta")
+}
