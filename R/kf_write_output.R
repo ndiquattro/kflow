7be80dd7138f0b7bcf722b716be7215ce9e788cf
Nick DiQuattro <ndiquattro@gmail.com>
2019-12-22 17:44:34 -0800

    Makes kf_write_output generic S3

--- R/kf_write_output.R
+++ R/kf_write_output.R
@@ -1,5 +1,9 @@
 #' Write output to a provided outputPath
 #'
+#' Writes content to a Kubeflow provided path for output.
+#'
+#' Defaults to [readr::write_file()], but uses [jsonlite::write_json()] or [readr::write_csv()] for appropriate classes.
+#'
 #' @param contents R object to save.
 #' @param outfile File provided by Kubeflow
 #'
@@ -11,8 +15,22 @@
 #' mock_path <- tempfile()
 #' kf_write_output(predictions_table_name, mock_path)
 kf_write_output <- function(contents, outfile) {
+  UseMethod("kf_write_output")
+}
+
+#' @export
+kf_write_output.default <- function(contents, outfile) {
   fs::dir_create(fs::path_dir(outfile))
   readr::write_file(contents, outfile)
+
+  outfile
+}
+
+# Write meta data to json
+#' @export
+kf_write_output.kflow_meta <- function(contents, outfile) {
+  fs::dir_create(fs::path_dir(outfile))
+  jsonlite::write_json(contents, outfile)
 
   outfile
 }
--- tests/testthat/test-kf_write_output.R
+++ tests/testthat/test-kf_write_output.R
@@ -6,3 +6,13 @@ test_that("File is saved and returned", {
   expect_equivalent(mock_file, saved_output)
   expect_true(file.exists(mock_file))
 })
+
+test_that("Correct method is used for kflow_meta class", {
+  mock_file <- tempfile()
+  test_metrics <- kf_init_metrics() %>% kf_add_metric("roc", .5, "RAW")
+
+  output <- kf_write_output(test_metrics, mock_file)
+
+  expect_true(file.exists(output))
+  expect_is(jsonlite::read_json(output), "list")
+})
