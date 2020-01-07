86e84d2ba278ebbf9c981b2cef15a519098f4603
Nick DiQuattro <ndiquattro@gmail.com>
2020-01-06 17:18:45 -0800

    Adds kf_make_component

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,13 @@
 find_kf_type <- function(x) {
-  x <- str_extract(x, "[^_]+$")
+  x <- stringr::str_extract(x, "[^_]+$")
 
   lookup <- list(
     string = "String",
-    num = "Numeric"
+    int = "Integer",
+    float = "Float",
+    bool = "Bool",
+    metrics = "Metrics",
+    uimeta = "UI_metadata"
   )
 
   lookup[[x]]
@@ -12,5 +16,5 @@ find_kf_type <- function(x) {
 find_arg_type <- function(x) {
   x <- stringr::str_extract(x, "[^_]+$")
 
-  ifelse(x %in% c("string", "num"), "inputValue", "outputPath")
+  ifelse(x %in% c("string", "int", "float", "bool"), "inputValue", "outputPath")
 }
